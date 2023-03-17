077e8f57985d0f76cdf4650eaa2292d5abd64bcc
Este Ban <dimascio47600@gmail.com>
2023-03-17 21:35:27 +0100

    presque fin du python

--- Level 2/Pierre_Papier_Ciseaux_Lezard_Spock.py
+++ Level 2/Pierre_Papier_Ciseaux_Lezard_Spock.py
@@ -2,50 +2,73 @@
 from random import randint
 import tkinter as tk
 
-num_players = 0
+PIERRE = 'Pierre'
+PAPIER = 'Papier'
+CISEAUX = 'Ciseaux'
+LEZARD = 'Lézard'
+SPOCK = 'Spock'
 
-def onePlayer():
-    global num_players
-    num_players = 1
-    start_game()
+options = [PIERRE, PAPIER, CISEAUX, LEZARD, SPOCK]
 
-def twoPlayer():
-    global num_players
-    num_players = 2
-    start_game()
+resultats = {
+    'Pierre': {
+        'Ciseaux': 'Joueur 1 gagne',
+        'Lézard': 'Joueur 1 gagne',
+        'Papier': 'Joueur 2 gagne',
+        'Spock': 'Joueur 2 gagne'
+    },
+    'Papier': {
+        'Pierre': 'Joueur 1 gagne',
+        'Spock': 'Joueur 1 gagne',
+        'Ciseaux': 'Joueur 2 gagne',
+        'Lézard': 'Joueur 2 gagne'
+    },
+    'Ciseaux': {
+        'Papier': 'Joueur 1 gagne',
+        'Lézard': 'Joueur 1 gagne',
+        'Pierre': 'Joueur 2 gagne',
+        'Spock': 'Joueur 2 gagne'
+    },
+    'Lézard': {
+        'Papier': 'Joueur 1 gagne',
+        'Spock': 'Joueur 1 gagne',
+        'Pierre': 'Joueur 2 gagne',
+        'Ciseaux': 'Joueur 2 gagne'
+    },
+    'Spock': {
+        'Pierre': 'Joueur 1 gagne',
+        'Ciseaux': 'Joueur 1 gagne',
+        'Papier': 'Joueur 2 gagne',
+        'Lézard': 'Joueur 2 gagne'
+    }
+}
 
-def start_game():
-    print("vous avez choisi ", num_players, "joueur(s)")
-    label.destroy()
-    start_button1.destroy()
-    start_button2.destroy()
-    if(num_players == 1):
-        game1()
-    elif(num_players == 2):
-        game2()
-    else:
-        print("erreur")
 
-def game1():
-    print("game1")
+def game():
+    label_game1 = tk.Label(root, text="Choisissez votre option : ")
+    label_game1.pack()
+    options = ["Pierre", "Papier", "Ciseaux", "Lézard", "Spock"]
+    selected_option = tk.StringVar()
+    selected_option.set(options[0])  # Par défaut, la première option est sélectionnée
+    menu = tk.OptionMenu(root, selected_option, *options)
+    menu.pack()
+    validate_button = tk.Button(root, text="Valider", command=lambda: combat(selected_option.get()))
+    validate_button.pack()
 
-def game2():
-    print("game2")
+def combat(joueur1):
+    joueur2 = options[randint(0, 4)]    
+    resultat = resultats[joueur1][joueur2]
+    # vide label
+    label_game2 = tk.Label(root, text="")
+    label_game2 = tk.Label(root, text=resultat)
+    label_game2.pack()   
+    
 
 # Créer une fenêtre principale
 root = tk.Tk()
 root.title("Choix du nombre de joueurs")
 
-# Ajouter un label pour expliquer ce qu'il faut faire
-label = tk.Label(root, text="Choisissez le nombre de joueur : ")
-label.pack()
-
-# Ajouter un bouton pour lancer le jeu
-start_button1 = tk.Button(root, text="1", command=onePlayer)
-start_button1.pack()
-start_button2 = tk.Button(root, text="2", command=twoPlayer)
-start_button2.pack()
-
+game()
 # Démarrer la boucle principale de la fenêtre
 root.mainloop()
 
